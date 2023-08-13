ddad7d2d1188874838b0f8fc948a0eb299cc5f0d
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-08-13 11:48:40 +0200

    Repair a clang warning and add copyrights

--- test/test_bessel_j_versus_boost.cpp
+++ test/test_bessel_j_versus_boost.cpp
@@ -1,3 +1,15 @@
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright Christopher Kormanyos 2022 - 2023.
+//  Distributed under the Boost Software License,
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#if (defined(__GNUC__) && defined(__clang__))
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#endif
+
 #include <iomanip>
 #include <iostream>
 
@@ -52,3 +64,7 @@ auto main() -> int
               << std::endl;
   }
 }
+
+#if (defined(__GNUC__) && defined(__clang__))
+#pragma GCC diagnostic pop
+#endif
